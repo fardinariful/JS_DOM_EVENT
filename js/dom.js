1ee3c4869a69174209d38dcd465cb97932f41e43
fardinariful <fardinewucse2020@gmail.com>
2024-07-01 10:56:22 +0600

    third commit

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,17 @@
 console.log('This is seperate js file');
+
+//option:2 onclick function
+function makered(){
+    console.log(document.body.style.backgroundColor='red');
+}
+
+
+
+//option :3
+const  makebluebutton=document.getElementById('make-blue');
+        makebluebutton.onclick=makeblue;
+
+
+        function makeblue(){
+            document.body.style.backgroundColor='blue';
+        }
